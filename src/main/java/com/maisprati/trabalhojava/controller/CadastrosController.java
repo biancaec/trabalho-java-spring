3a68220d01ff9517685a8d6c464d0eadda3ff2a8
biancaec <biancaec@gmail.com>
2022-06-06 13:10:29 -0300

    requisitando um cadastro ao db

--- src/main/java/com/maisprati/trabalhojava/controller/CadastrosController.java
+++ src/main/java/com/maisprati/trabalhojava/controller/CadastrosController.java
@@ -1,5 +1,24 @@
 package com.maisprati.trabalhojava.controller;
 
-public class CadastrosController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.maisprati.trabalhojava.model.Cadastros;
+import com.maisprati.trabalhojava.repository.CadastrosRepository;
+
+@RestController
+@RequestMapping("/cadastros")
+public class CadastrosController {
+	
+	@Autowired
+	private CadastrosRepository repository;
+	
+	@GetMapping
+	public List<Cadastros> findAll() {
+		return repository.findAll();
+	}
 }
